deb0bbdc7117090221a7dd74e2ec88ed8cfe8c4f
Darren Jian <darrenjian1@gmail.com>
2020-09-13 14:53:39 -0700

    Updates

---
+++ precache-manifest.cf18368fb309f26370f34cec23ea0eec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2342356a0fe8a3d2934e9d0b277ef135",
+    "url": "/DarrenJian1/haubi.git/index.html"
+  },
+  {
+    "revision": "247ffd3ba4214d5de611",
+    "url": "/DarrenJian1/haubi.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90bc83ad0502e498d610",
+    "url": "/DarrenJian1/haubi.git/static/js/2.cdab9428.chunk.js"
+  },
+  {
+    "revision": "903c93e020d6d8c7ad4f3a4d8273fb58",
+    "url": "/DarrenJian1/haubi.git/static/js/2.cdab9428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "247ffd3ba4214d5de611",
+    "url": "/DarrenJian1/haubi.git/static/js/main.eea513e8.chunk.js"
+  },
+  {
+    "revision": "d5bfc0c52476459dbf6f",
+    "url": "/DarrenJian1/haubi.git/static/js/runtime-main.6e78be01.js"
+  }
+]);
